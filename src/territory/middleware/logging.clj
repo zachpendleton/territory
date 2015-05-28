6acb215d0bc1e02b19bc328d0d8386920c8a1326
Zach Pendleton <zachpendleton@gmail.com>
2015-05-28 17:19:25 -0600

    clean up logging format
    lines are now: "GET /health-check HTTP/1.1" 200

--- src/territory/middleware/logging.clj
+++ src/territory/middleware/logging.clj
@@ -1,11 +1,13 @@
 (ns territory.middleware.logging
-  (:require [taoensso.timbre :refer [info]]))
+  (:require [taoensso.timbre :refer [info]]
+            [clojure.string :refer [upper-case]]))
 
 (defn wrap-logger
   [app]
   (fn [req]
     (let [res (app req)
           {:keys [status body]} res
-          {:keys [content-type uri]} req]
-      (info status uri content-type)
+          {:keys [content-type uri request-method]} req
+          request-method (-> request-method name upper-case)]
+      (info (format "\"%s %s HTTP/1.1\" %d" request-method uri status))
       res)))
